e7633f9ebebecdc2babb283d80ab0e6360edca19
Julia K <korchkova.yulia@gmail.com>
2024-06-09 00:24:54 +0300

    style: Apply eslint fixes
    Applied eslint errors and warnings fixed as well as disabled
    typescript-eslint/no-var-requires rule for webpack configurationn files
    due to the need of use require instead of import statement

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
-const commonConfig = require("./webpack.config");
-const {merge} = require("webpack-merge");
+const commonConfig = require('./webpack.config');
+const {merge} = require('webpack-merge');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(commonConfig, {
-  mode: "production",
   devtool: 'source-map',
-  output: {
-    filename: 'js/[name].[chunkhash].js',
-    chunkFilename: 'js/[name].[chunkhash].js',
-    assetModuleFilename: "css/[name][chunkhash][ext]",
-  },
+  mode: 'production',
   module: {
     rules: [
       {
@@ -31,12 +27,17 @@ module.exports = merge(commonConfig, {
       },
     ]
   },
+  output: {
+    assetModuleFilename: 'css/[name][chunkhash][ext]',
+    chunkFilename: 'js/[name].[chunkhash].js',
+    filename: 'js/[name].[chunkhash].js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-      minify: true,
-      hash: true,
       cache: true,
+      hash: true,
+      minify: true,
+      template: path.join(__dirname, 'public', 'index.html'),
     })
   ],
 })
