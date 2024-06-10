5bbf51258a2596336604a75c61c786fa84674064
Julia K <korchkova.yulia@gmail.com>
2024-06-10 23:21:46 +0300

    feat: integrate css constants and global styles
    The previously added hooks were added to global.ts file which designed
    for global css styles as well as css reset rules for the App
    Global styles were integrated into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import GlobalStyle from './global';
 import { router } from './router';
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         {router.map(({ Component, path }) => {
           return <Route Component={Component} key={path} path={path}></Route>;
---
+++ src/global.ts
@@ -0,0 +1,42 @@
+import '@constants/css';
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  html {
+    height: 100%;
+    font-size: calc(8px + 0.39vw);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Lexend Deca', 'Inter', sans-serif;
+    transition: all .4s ease-out;
+  }
+
+  #root{
+    margin: 0 auto;
+    isolation: isolate;
+    background: var(--c-palette-color-white-2);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+`;
