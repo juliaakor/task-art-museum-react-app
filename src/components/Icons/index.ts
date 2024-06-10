1a085f2259d0f2e0519114440bd092a63efbdd7d
Julia K <korchkova.yulia@gmail.com>
2024-06-11 01:25:30 +0300

    feat: add burger menu icon and new relative paths
    - Added burgerMenuIcon for mobile view
    - Added the following relative paths: components, utils
    - Additionally updated hooks-palette css variables

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,5 +1,6 @@
 export { ArrowIcon } from './arrowIcon';
 export { BookmarkIcon } from './bookmarkIcon';
+export { BurgerMenuIcon } from './burgerMenuIcon';
 export { HomeIcon } from './homeIcon';
 export { ModsenLogoIcon } from './modsenLogoIcon';
 export { MuseumLogoIcon } from './museumLogoIcon';
