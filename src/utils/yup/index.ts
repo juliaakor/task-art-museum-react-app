f91765ec5591f2d080a95e03f475516d8b62231c
Julia K <korchkova.yulia@gmail.com>
2024-06-12 01:17:47 +0300

    feat: add utils yup schemas for api data
    The following schemas have been added to validate the information
    received from the api and to remove data from unnecessary data and
    fields: artworkShortSchema (short info for cards), PaginationInfoSchema
    (fields for pagination), PaintingsListSchema (schema for raw data from
    api), FullPaintingInfoSchema (full schema to display detailed info)

---
+++ src/utils/yup/index.ts
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+
+const isRequiredString = Yup.string().required().nullable();
+const isRequiredBoolean = Yup.boolean().required();
+const isRequiredNumber = Yup.number().required();
+
+export const artworkShortSchema = Yup.object().shape({
+  artist_title: isRequiredString,
+  id: isRequiredNumber,
+  image_id: isRequiredString,
+  is_public_domain: isRequiredBoolean,
+  title: isRequiredString,
+});
+
+export const PaginationInfoSchema = Yup.object().shape({
+  current_page: isRequiredNumber,
+  limit: isRequiredNumber,
+  next_url: isRequiredString,
+  offset: isRequiredNumber,
+  total: isRequiredNumber,
+  total_pages: isRequiredNumber,
+});
+
+export const PaintingsListSchema = Yup.object().shape({
+  data: Yup.array(artworkShortSchema),
+  pagination: PaginationInfoSchema,
+});
+
+export const FullPaintingInfoSchema = Yup.object().shape({
+  artist_display: isRequiredString,
+  artist_title: isRequiredString,
+  credit_line: isRequiredString,
+  dimensions: isRequiredString,
+  fiscal_year: isRequiredNumber,
+  gallery_title: isRequiredString,
+  id: isRequiredString,
+  image_id: isRequiredString.nonNullable(),
+  is_public_domain: isRequiredBoolean,
+  place_of_origin: isRequiredString,
+  title: isRequiredString,
+});
+
+export type FullPaintingInfoType = Yup.InferType<typeof FullPaintingInfoSchema>;
