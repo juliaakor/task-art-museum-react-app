26cdfa366f81738f51219a37381485f6e67ec22d
Julia K <korchkova.yulia@gmail.com>
2024-06-11 19:33:49 +0300

    feat: add styled search component n types file
    - Added a types.d.ts file with the following types: PaintingCardInfoType
    (short information type needed to display painting cards),
    PaginationInfoType (field types returned from the pagination api),
    PaintingsListType (basic information to display a painting card and its
    component)
    - Added Search component with styles file that uses Formik to make the
    search form and integrated useDebounce logic for the search form

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,86 @@
+import { SearchIcon } from '@components/Icons';
+import { Input } from '@components/common';
+import { getBaseApiUrl } from '@utils/api';
+import { useDebounce } from '@utils/hooks';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React, { useCallback, useEffect, useState } from 'react';
+import { PaintingsListType } from 'types';
+import { Schema } from 'yup';
+
+import { CardListWrapper, FieldWrapper, SearchError, SearchWrapper, SubmitIcon } from './styled';
+
+interface SearchProps {
+  children?: React.ReactNode;
+  initialValues: SearchFormValues;
+  placeholder?: string;
+  validationSchema: Schema;
+}
+
+interface SearchFormValues {
+  query: string;
+}
+
+export const Search = ({ children, initialValues, placeholder, validationSchema }: SearchProps) => {
+  const [searchResults, setSearchResults] = useState<PaintingsListType>();
+  const [query, setQuery] = useState('');
+
+  const handleSearch = useCallback((query: string) => {
+    setQuery(query);
+  }, []);
+
+  useEffect(() => {
+    if (!query || searchResults) return;
+    const fetchPaintings = async () => {
+      const response = await fetch(`${getBaseApiUrl(query)}`);
+      const data = await response.json();
+      setSearchResults(data);
+    };
+
+    fetchPaintings();
+  }, [query, searchResults]);
+
+  const debouncedSearch = useDebounce({ cb: handleSearch, delay: 300 });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        handleSearch(values.query);
+      }}
+      validationSchema={validationSchema}
+    >
+      {({ handleChange, handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
+          <SearchWrapper>
+            <FieldWrapper>
+              <Field name="query">
+                {({ field }: { field: React.InputHTMLAttributes<SearchProps> }) => (
+                  <Input
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      handleChange(e);
+                      debouncedSearch(e.target.value);
+                    }}
+                    placeholder={placeholder}
+                    value={field.value}
+                    {...field}
+                  />
+                )}
+              </Field>
+              <SubmitIcon type="submit">
+                <SearchIcon size={18} />
+              </SubmitIcon>
+            </FieldWrapper>
+            <ErrorMessage
+              component="div"
+              name="query"
+              render={() => (
+                <SearchError>Search is empty! Cant find anything? Scroll below for more inspiration...</SearchError>
+              )}
+            />
+            {children}
+          </SearchWrapper>
+        </Form>
+      )}
+    </Formik>
+  );
+};
---
+++ src/components/Search/styled.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const SearchWrapper = styled.div`
+  width: 50%;
+  margin: var(--c-spacing-xx-small) auto;
+`;
+
+export const FieldWrapper = styled.div`
+  position: relative;
+`;
+
+export const SubmitIcon = styled.button`
+  border: 0;
+  background: transparent;
+  border-radius: 50%;
+  padding: var(--c-spacing-xx-small);
+  position: absolute;
+  right: var(--c-spacing-xxx-small);
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+
+  &:hover {
+    background: var(--c-palette-color-orange-3-30);
+  }
+`;
+
+export const SearchError = styled.span`
+  color: var(--c-palette-color-orange-3-70);
+  margin: var(--c-spacing-x-small);
+  font-size: var(--c-font-weight-thin);
+`;
