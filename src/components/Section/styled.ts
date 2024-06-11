875b47782b0dbf1c3b606325a19fc215ccf87c87
Julia K <korchkova.yulia@gmail.com>
2024-06-11 21:14:10 +0300

    feat: add reusable styled section component
    Added a reusable Section component that uses the title and information
    and can be used as a wrapper for the entire section

---
+++ src/components/Section/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const SectionContainer = styled.section`
+  margin: 0 0 var(--c-spacing-xxx-large) 0;
+`;
+
+export const SubSectionName = styled.h3`
+  font-size: var(--c-font-size-2);
+  padding: 0;
+  color: var(--c-palette-color-orange-2);
+  margin: 0 auto;
+  width: max-content;
+`;
+
+export const SectionName = styled.div`
+  font-size: var(--c-font-size-6);
+  padding: 0 0 var(--c-spacing-xxx-medium) 0;
+  color: var(--c-palette-color-gray-3);
+  margin: 0 auto var(--c-spacing-xxx-medium) auto;
+  width: max-content;
+`;
