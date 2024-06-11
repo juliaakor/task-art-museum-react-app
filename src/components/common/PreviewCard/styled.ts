5e69ed1bac7a081fb06cb35f1d350ef0aca4b1f0
Julia K <korchkova.yulia@gmail.com>
2024-06-11 20:06:01 +0300

    feat: add common preview card component n helpers
    The following changes have been made to add the PreviewCard component
    with its style file:
      - Added a helper function getPaintingImageUrl to get the api url of
    images to render in the component
      - Added a notFoundIcon component to be used when a painting image was
    not retrieved successfully or if there was no image (for aggregated
    search queries)
      - Updated imports for utils api and common components to export new
    functionality
      - Added new css rules to the css constants index file for future use
      - Added PreviewCard component with its own styles to display a card
    with brief information about the picture, which comes in two versions
    full size and mini size with picture image. The component uses
    FullSizeCardDefaults and MiniSizeCardDefaults to control the dimensions
    of the full-size and miniature view

---
+++ src/components/common/PreviewCard/styled.ts
@@ -0,0 +1,68 @@
+import { FlexVerticalCenter, TextLengthWrap } from '@constants/css';
+import styled, { css } from 'styled-components';
+
+interface PreviewCardProps {
+  $position?: string;
+  $size?: string;
+}
+
+export const PreviewCardContainer = styled.div<PreviewCardProps>`
+  background: var(--c-palette-color-white-1);
+  padding: var(--c-spacing-medium) 1.5rem;
+  ${FlexVerticalCenter}
+  justify-content: space-between;
+  width: ${({ $size }) => `${$size}`};
+  position: ${({ $position }) => `${$position}`};
+  bottom: -10%;
+`;
+
+export const CardArtist = styled.h5`
+  color: var(--c-palette-color-orange-2);
+  font-size: var(--c-font-size-2);
+  font-weight: var(--c-font-weight-regular);
+  margin-bottom: var(--c-spacing-small);
+  ${TextLengthWrap}
+`;
+
+export const CardTitle = styled.h4`
+  color: var(--c-palette-color-gray-3);
+  font-size: var(--c-font-size-3);
+  font-weight: var(--c-font-weight-medium);
+  ${TextLengthWrap}
+`;
+
+export const CardInfo = styled.div`
+  margin-right: var(--c-spacing-xxx-small);
+  width: 50%;
+`;
+
+export const CardStatus = styled.span`
+  color: var(--c-palette-color-gray-3);
+  font-size: var(--c-font-size-2);
+  font-weight: var(--c-font-weight-bold);
+`;
+
+export const IconWrapper = styled.span`
+  background: var(--c-palette-color-white-3);
+  border-radius: 35px;
+  padding: var(--c-spacing-medium);
+
+  &:hover {
+    background: var(--c-palette-color-orange-3-30);
+  }
+`;
+
+const Painting = css`
+  width: var(--c-size-x-small);
+  height: var(--c-size-x-small);
+  object-fit: cover;
+  padding: var(--c-spacing-small);
+`;
+
+export const PaintingImage = styled.img`
+  ${Painting}
+`;
+
+export const PaintingDefaultWrapper = styled.span`
+  ${Painting}
+`;
--- src/constants/css/index.ts
+++ src/constants/css/index.ts
@@ -1,6 +1,11 @@
 import { css } from 'styled-components';
 
 import './hooks.css';
+export const TextLengthWrap = css`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
 export const FlexVerticalCenter = css`
   display: flex;
