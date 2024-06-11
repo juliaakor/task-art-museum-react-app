49ccbebb1d9fc5c552bae4eb3bbf3197a6228a7c
Julia K <korchkova.yulia@gmail.com>
2024-06-12 01:25:36 +0300

    feat: add reusable overview component for details
    Overview component have been added for DetailInfo page to display
    additional info about the painting. Component also uses
    FullPaintingInfoType as props type

---
+++ src/components/Overview/index.tsx
@@ -0,0 +1,34 @@
+import { SubHeading, TextHighlightWrapper } from '@constants/css';
+import { FullPaintingInfoType } from '@utils/yup';
+import React from 'react';
+
+import { PropertyWrapper } from './styled';
+
+export const Overview = ({
+  credit_line,
+  dimensions,
+  gallery_title,
+  is_public_domain,
+  place_of_origin,
+}: FullPaintingInfoType) => {
+  return (
+    <div>
+      <SubHeading>Overview</SubHeading>
+      <PropertyWrapper>
+        <TextHighlightWrapper color="var(--c-palette-color-orange-2)">Artist nationality:</TextHighlightWrapper>{' '}
+        {place_of_origin}
+      </PropertyWrapper>
+      <PropertyWrapper>
+        <TextHighlightWrapper color="var(--c-palette-color-orange-2)">Dimensions: Sheet:</TextHighlightWrapper>{' '}
+        {dimensions}
+      </PropertyWrapper>
+      <PropertyWrapper>
+        <TextHighlightWrapper color="var(--c-palette-color-orange-2)">Credit Line:</TextHighlightWrapper> {credit_line}
+      </PropertyWrapper>
+      <PropertyWrapper>
+        <TextHighlightWrapper color="var(--c-palette-color-orange-2)">Repository:</TextHighlightWrapper> {gallery_title}
+      </PropertyWrapper>
+      <PropertyWrapper>{is_public_domain ? 'Public' : 'Private'}</PropertyWrapper>
+    </div>
+  );
+};
---
+++ src/components/Overview/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PropertyWrapper = styled.div`
+  color: var(--c-palette-color-gray-3);
+  font-size: var(--c-font-size-2);
+  font-weight: var(--c-font-weight-regular);
+  line-height: var(--c-line-height-text-2);
+  padding: 0 0 var(--c-spacing-medium) 0;
+`;
