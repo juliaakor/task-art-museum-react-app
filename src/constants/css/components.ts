20a5a6ac264e7dfd5d5f7514ced0b4c53f0b1262
Julia K <korchkova.yulia@gmail.com>
2024-06-12 01:32:52 +0300

    feat: setup detail info page
    - The DetailInfo page was configured to use the id of the picture to load
    information about it from the page parameters.
    - Additionally, SubHeading and IconButtonWrapper were added to reuse the
    bookmark button throughout the application

--- src/constants/css/components.ts
+++ src/constants/css/components.ts
@@ -10,6 +10,24 @@ export const PageHeading = styled.h1`
   font-size: var(--c-font-size-7);
 `;
 
+export const SubHeading = styled.h4`
+  font-size: var(--c-font-size-6);
+  font-weight: var(--c-font-weight-regular);
+  color: var(--c-palette-color-gray-3);
+  padding: 0 0 var(--c-spacing-xxx-medium) 0;
+  line-height: var(--c-line-height-text-8);
+`;
+
+export const IconButtonWrapper = styled.span`
+  background: var(--c-palette-color-white-3);
+  border-radius: 35px;
+  padding: var(--c-spacing-medium);
+
+  &:hover {
+    background: var(--c-palette-color-orange-3-30);
+  }
+`;
+
 interface TextHighlightWrapperProps {
   color: string;
 }
--- src/pages/DetailInfo/index.tsx
+++ src/pages/DetailInfo/index.tsx
@@ -1,5 +1,70 @@
-import React from 'react';
+import { BookmarkIcon } from '@components/Icons';
+import { Loader } from '@components/Loader';
+import { Overview } from '@components/Overview';
+import { SubHeading } from '@constants/css';
+import { ROUTES } from '@constants/routes';
+import { getPaintingImageUrl } from '@utils/api';
+import { FullPaintingInfoSchema, FullPaintingInfoType } from '@utils/yup';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import {
+  ArtistName,
+  ButtonWrapper,
+  DetailsContainer,
+  OverviewContainer,
+  PaintingImage,
+  PaintingWrapper,
+  YearsLabel,
+} from './styled';
 
 export const DetailInfoPage = () => {
-  return <></>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [data, setData] = useState<FullPaintingInfoType | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
+        const fetchedData = await response.json();
+
+        const validatedData = await FullPaintingInfoSchema.validate(fetchedData.data, { stripUnknown: true });
+
+        setData(validatedData);
+      } catch (error) {
+        navigate(ROUTES.ERROR_OCCURRED);
+      }
+    };
+
+    getData();
+  }, [id, navigate]);
+
+  const handleBookmarkClick = () => {};
+
+  return (
+    <DetailsContainer>
+      {data ? (
+        <>
+          <PaintingWrapper>
+            <PaintingImage src={getPaintingImageUrl(data.image_id)} />
+            <ButtonWrapper onClick={() => handleBookmarkClick()}>
+              <BookmarkIcon />
+            </ButtonWrapper>
+          </PaintingWrapper>
+          <OverviewContainer>
+            <div>
+              <SubHeading>{data.title}</SubHeading>
+              <ArtistName>{data.artist_title}</ArtistName>
+              <YearsLabel>{data.fiscal_year}</YearsLabel>
+            </div>
+            <Overview {...data} />
+          </OverviewContainer>
+        </>
+      ) : (
+        <Loader />
+      )}
+    </DetailsContainer>
+  );
 };
