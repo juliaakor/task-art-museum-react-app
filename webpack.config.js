29b99f5f30353450889196e25f2e57159d420f15
Julia K <korchkova.yulia@gmail.com>
2024-06-12 07:16:26 +0300

    feat: add esbuild-loader and webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
 module.exports = {
   entry: {
@@ -32,10 +33,42 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    mergeDuplicateChunks: true,
+    minimize: true,
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: 'es2016',
+      }),
+    ],
+    removeEmptyChunks: true,
+    sideEffects: false,
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          chunks: 'all',
+          enforce: true,
+          name: (module) => {
+            const [, match] = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]([^\\/]*)([\\/]([^\\/]*))?([\\/]([^\\/]*))?|$)/
+            );
+
+            return `vendors/${match.replace('@', '')}`;
+          },
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+      chunks: 'all',
+    },
+  },
   output: {
     clean: true,
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
+  },
+  performance: {
+    maxAssetSize: 1024 ** 2,
+    maxEntrypointSize: Infinity,
   },
   plugins: [new MiniCssExtractPlugin()],
   resolve: {
