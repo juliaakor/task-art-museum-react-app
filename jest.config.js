8c0281862bd7a1bf0a9499ad704aee5a1c0ace47
Julia K <korchkova.yulia@gmail.com>
2024-06-18 17:31:21 +0300

    feat: add jest packages and config file
      - Added the following packages for testing: jest, types/jest
      - Added jest config and set the required coverageThreshold,
    moduleFileExtensions and moduleNameMapper to access files easier
      - Added new scripts to package.json: test, test:all, test:ci,
    test:coverage

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@type/(.*)$': '<rootDir>/src/type/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@validation/(.*)$': '<rootDir>/src/validation/$1',
+  },
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
